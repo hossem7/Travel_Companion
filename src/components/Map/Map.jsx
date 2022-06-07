d56145118ea4754c5deb5d2307f1427fd9af89e7
hossem7 <mohammad.hossein0999@gmail.com>
2022-06-07 15:12:26 -0400

    Setting up map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocalActivityOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  
   return (
     <div>Map</div>
   );
