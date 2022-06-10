9d313da6e17e8a290963a2fd2e4f99570547aa21
hossem7 <mohammad.hossein0999@gmail.com>
2022-06-09 22:33:52 -0400

    Environment Variables

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,7 @@ const Map = () => {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDUEMocmXkBqDZtTAHkEfuc6776mjjWmKY' }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
           defaultCenter={coordinates}
           center={coordinates}
           defaultZoom={14}
