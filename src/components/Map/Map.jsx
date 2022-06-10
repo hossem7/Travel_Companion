f47b2ecf7c32ad366da858018ff3f05e5ff3dd62
hossem7 <mohammad.hossein0999@gmail.com>
2022-06-09 22:16:38 -0400

    Map Implementation and Styling

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocalActivityOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
@@ -9,9 +9,25 @@ import useStyles from './styles';
 const Map = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
-  
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDUEMocmXkBqDZtTAHkEfuc6776mjjWmKY' }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin = {[50, 50, 50, 50]}
+          options={''}
+          onChange={''}
+          onChildClick={''}
+      >
+
+      </GoogleMapReact>
+
+    </div>
   );
 }
 
